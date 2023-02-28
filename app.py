39a84e041a5d16dc68945f10b49e40c773196c34
Jess Stevenson <73757176+jws168@users.noreply.github.com>
2023-02-27 18:35:13 -0600

    added code to make it easier to run

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import sys
 from flask import Flask, render_template
 app = Flask(__name__)
 
-#run by using command 'python -m flask run' in terminal
 
 @app.route("/")
 def home():
     return render_template('test.html')
+
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
